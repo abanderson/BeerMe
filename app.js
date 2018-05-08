e488cef2f84408dfd0bbb4a4410f010109c4bcbf
Alex Anderson <alex@abanderson.com>
2018-05-07 21:59:35 -0400

    Fixed 'Access-Control-Allow-Origin' header error in Chrome

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 var ba = require('beeradvocate-api');
 const app = express();
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/beers', function(req, res) {
     ba.beerSearch(req.query.search, function(beers) {
         res.send(beers);
