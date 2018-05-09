5336385c35f720f8077e28f9c7619fd3e0d5bf58
Alex Anderson <alex@abanderson.com>
2018-05-09 10:56:52 -0400

    Refactored code a little bit. Added .catch for Promise and changed sidebar implementation slightly

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 $(function() {
     let api = new Api(33.748, -84.392);
+    let sidebar = new SideBar();
     let breweryList = [];
     let beerList = [];
+
 
     api.searchForBreweries()
         .then(function(breweries) {
             breweryList = breweries;
-            //console.log(breweryList);
             //Render Map here
             breweries.forEach(function(brewery) {
                 api.getBeersForBrewery(brewery.name)
                     .then(function(beers) {
                         beers.forEach(function(beer) {
                             beerList.push(beer);
-                            let sidebar = new SideBar(beerList);
-                            sidebar.renderSideBar();
+                            sidebar.renderSideBar(beerList);
                         });
-                        //console.log(beerList);
+                    })
+                    .catch(function(error) {
+                        console.log(error);
                     });
             });
         })
